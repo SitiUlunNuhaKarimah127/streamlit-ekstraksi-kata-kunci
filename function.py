0d673870eee96f31664822b65f3c6265b7b6c744
arosyihuddin <arosyihuddin6@gmail.com>
2023-12-13 17:56:20 +0700

    add all file

---
+++ function.py
@@ -0,0 +1,89 @@
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+import streamlit as st
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
+import re
+import nltk
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
+def cleaning(text):
+  text = re.sub(r'[^\w\s.?!,]', '', text).strip().lower()
+  return text
+
+corpus = stopwords.words('indonesian')
+def stopwords(text):
+  return ' '.join(word for word in text.split() if word not in corpus)
+
+def tokenizer(text):
+  text = text.lower()
+  return sent_tokenize(text)
+
+def plot_graph(G, figsize=(35, 30), node_size=700, node_color='skyblue'):
+  # Menggambar graf dengan canvas yang diperbesar
+  pos = nx.spring_layout(G)  # Menentukan posisi simpul
+  labels = nx.get_edge_attributes(G, 'weight')
+
+  # Menentukan ukuran canvas
+  fig = plt.figure(figsize=figsize)
+
+  # Menggambar graf dengan ukuran canvas yang diperbesar
+  nx.draw(G, pos, with_labels=True, node_size=node_size, node_color=node_color)
+  nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_color='red')
+  # plt.show()
+  st.pyplot(fig)
+  
+
+def graph_co_occurrence(x, threshold=0, show_matrics = False):
+  vectorizer = CountVectorizer()
+  tfidf_matrics = vectorizer.fit_transform(x)
+  co_occurrence_matrix = tfidf_matrics.T.dot(tfidf_matrics).toarray()
+  df_co_occurence = pd.DataFrame(co_occurrence_matrix, columns=vectorizer.get_feature_names_out())
+  df_co_occurence.insert(0, 'Words', vectorizer.get_feature_names_out())
+  
+  st.session_state.co_occurence = df_co_occurence
+
+  G = nx.DiGraph()
+
+  # Menambahkan edge ke graf berdasarkan matriks co-occurrence
+  for i in range(len(co_occurrence_matrix)):
+    for j in range(i + 1, len(co_occurrence_matrix)):
+      weight = co_occurrence_matrix[i, j]
+      if weight > threshold:
+        G.add_edge(vectorizer.get_feature_names_out()[i], vectorizer.get_feature_names_out()[j], weight=weight)
+  return G
+
+def extract_top_words(x, w=3, threshold=0, show_matrics=False, index=1):
+    full_text = ' '.join(word for word in x)
+
+    G = graph_co_occurrence(x, threshold, show_matrics)
+
+    # Menghitung nilai dari PageRank (TextRank)
+    scores = nx.pagerank(G)
+
+    # Dictionary untuk menyimpan skor tertinggi setiap kata
+    ranked_words_dict = {}
+
+    for word in ' '.join(x).split():
+        current_score = scores.get(word, 0)
+        if word not in ranked_words_dict or current_score > ranked_words_dict[word]:
+            ranked_words_dict[word] = current_score
+
+    # Mengurutkan kata-kata berdasarkan skor tertinggi
+    ranked_words = sorted(((score, word) for word, score in ranked_words_dict.items()), key=lambda x: (x[0], x[1]), reverse=True)
+
+    # Memilih sejumlah w kata tertinggi
+    selected_words = [word for _, word in sorted(ranked_words[:w], key=lambda x: x[1])] if w is not None else None
+
+    # Menggabungkan kata-kata menjadi satu string terpisah dengan koma
+    keywords = ', '.join(selected_words) if selected_words else ''
+    
+    st.session_state.ranked_words = ranked_words
+    st.session_state.full_text = full_text
+
+    return (G, keywords)
---
+++ main.py
@@ -0,0 +1,57 @@
+from streamlit_option_menu import option_menu
+from function import *
+
+st.header("Ekstraksi Kata Kunci Artikel Berita", divider='rainbow')
+
+num_word = int(st.number_input("Jumlah Kata Kunci", 1))
+text = st.text_area("Masukkan Artikel Berita")
+button = st.button("Ekstrak Kata Kunci")
+
+if "keywoards" not in st.session_state:
+    st.session_state.keywoards = []
+    st.session_state.graph = []
+    st.session_state.co_occurence = ""
+
+if button:
+  text_clean = cleaning(text)
+  stopwordsRemoval = stopwords(text_clean)
+  tokenizing = tokenizer(stopwordsRemoval)
+  G, keywoards = extract_top_words(x=tokenizing, w=num_word)
+  
+  # Menambahkan hasil keywoards ke dalam session state
+  st.session_state.keywoards = keywoards
+  st.session_state.graph = G
+  st.session_state.num_word = num_word
+
+    
+selected = option_menu(
+  menu_title="",
+  options=["Keywoards", "Graph Keywoards", "Co-occurence Matrics"],
+  icons=["data", "Process", "model", "implemen", "Test", "sa"],
+  orientation="horizontal"
+  )
+
+if selected == "Keywoards":
+  if st.session_state.keywoards:
+    st.write(f'**{st.session_state.num_word} Kata Kunci : {st.session_state.keywoards}**')
+    st.write(f'Dokumen : {st.session_state.full_text}')
+    st.write("TextRank Scores All Words:")
+    for score, word in st.session_state.ranked_words:
+        st.write(f"Skor: {score} -> Kata: {word}")
+  
+elif selected == "Graph Keywoards":
+  col1, col2 = st.columns(2)
+  with col1:
+    x_canvas = st.number_input('Lebar Canvas', 10)
+  
+  with col2:
+    y_canvas = st.number_input('Panjang Canvas', 10)
+    
+  node_size = st.number_input('Node Size', 400)
+  
+  if st.session_state.graph != []:
+    plot_graph(st.session_state.graph, (x_canvas, y_canvas), node_size)
+  
+elif selected == "Co-occurence Matrics":
+  if str(type(st.session_state.co_occurence)) == "<class 'pandas.core.frame.DataFrame'>":
+    st.dataframe(st.session_state.co_occurence, use_container_width=True)
